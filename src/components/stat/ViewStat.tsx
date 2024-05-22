73b055d6209648c7d1c302104f0101b600193a9a
fahimahammed <fahimahammed.cse@gmail.com>
2024-05-23 01:50:54 +0600

    updated

--- src/components/stat/ViewStat.tsx
+++ src/components/stat/ViewStat.tsx
@@ -1,10 +1,105 @@
 import React from 'react';
+import { Doughnut, Line, Pie } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    ArcElement,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Col, Row } from 'antd';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+///const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const lineData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [200, 350, 280, 520, 400, 680],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: [150, 220, 300, 450, 320, 500],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+
+export const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
 
 const ViewStat = ({ base }: { base?: string }) => {
     return (
         <div>
-            hello
             {base}
+            <Row gutter={24}>
+                <Col span={8}>
+                    <Doughnut data={data} />
+                </Col>
+                <Col span={8}>
+                    <Pie data={data} />
+                </Col>
+                <Col span={8}>
+                    <Line options={options} data={data} />
+                </Col>
+            </Row>
         </div>
     );
 };
